76c8cab66ec3db06748a2e4745027828e1b0e5cc
Sanjay <121040100+sanjay-1458@users.noreply.github.com>
2023-09-23 13:00:24 +0530

    Update Key Pair.cpp

--- Hashing/Key Pair.cpp
+++ Hashing/Key Pair.cpp
@@ -42,3 +42,30 @@ public:
 	    return false;
 	}
 };
+
+
+class Solution{
+public:	
+	// Function to check if array has 2 elements
+	// whose sum is equal to the given value
+	bool hasArrayTwoCandidates(int arr[], int n, int x) {
+	    // code here
+	    unordered_map<int,int> mp;
+	    for(int i=0;i<n;++i){
+	        mp[arr[i]]++;
+	    }
+	    for(auto it:mp){
+	        int a=it.first;
+	        int b=x-a,c=it.second;
+	        if(a==b){
+	            if(c>1){
+	                return true;
+	            }
+	        }
+	        else if(mp.find(b)!=mp.end()){
+	            return true;
+	        }
+	    }
+	    return false;
+	}
+};
