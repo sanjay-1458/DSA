e71aa61a8d83f48f9083d20d37c84d267d9e829a
Sanjay <121040100+sanjay-1458@users.noreply.github.com>
2023-09-23 12:30:12 +0530

    Update Intersection of two arrays.cpp

--- Hashing/Intersection of two arrays.cpp
+++ Hashing/Intersection of two arrays.cpp
@@ -20,3 +20,31 @@ class Solution {
         return s1.size();
     }
 };
+
+
+
+
+class Solution {
+  public:
+    // Function to return the count of the number of elements in
+    // the intersection of two arrays.
+    int NumberofElementsInIntersection(int a[], int b[], int n, int m) {
+        // Your code goes here
+        unordered_set<int> s;
+        int cnt=0;
+        for(int i=0;i<n;++i){
+            int key=a[i];
+            if(s.find(key)==s.end()){
+                s.insert(key);
+            }
+        }
+        for(int i=0;i<m;++i){
+            int key=b[i];
+            if(s.find(key)!=s.end()){
+                cnt++;
+                s.erase(key);
+            }
+        }
+        return cnt;
+    }
+};
