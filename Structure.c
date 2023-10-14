3244728a3f57d36de552b6bd04ca204783773cd5
Sanjay <121040100+sanjay-1458@users.noreply.github.com>
2023-10-14 19:23:59 +0530

    Create Structure.c

---
+++ Structure.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+// Syntax
+/*
+struct structure_name{
+    datatype1 member_name1;
+    datatype2 member_name2;             -> no memory is allocated during declaration
+    datatype3 member_name3;
+    ...
+};
+
+we have to define its instance to use structure in our program
+
+struct structure_name{
+    datatype1 member_name1;
+    datatype2 member_name2;
+    ...
+}variable1,variable2;
+
+or 
+
+struct structure_name variable1,variable2;
+
+we can access structure member using the dot operator
+
+structure_name.member1;
+structure_name.member2;
+
+*/
+
+
+
+int main()
+{
+
+    return 0;
+}
