c71aae3b35b3c4e331f52b998b7f4160519ed3d0
Sanjay <121040100+sanjay-1458@users.noreply.github.com>
2023-10-15 12:18:32 +0530

    Update OOP's.cpp

--- OOP's.cpp
+++ OOP's.cpp
@@ -46,4 +46,92 @@ int main()
 }
 
 
+///////////////////////////////////////////////////////////////////////////////////
 
+#include <bits/stdc++.h>
+using namespace std;
+
+class Hero{
+
+private:
+    int health;
+    char level;
+
+public:
+    char *name;
+    // Default Constructor
+    Hero(){
+        cout<<"Default Constructor"<<endl;
+        name=new char[100];
+    }
+    // Parameterised Constructor
+    Hero(int health){
+        this->health=health;
+    }
+    Hero(int health,char level){
+        this->health=health;
+        this->level=level;
+    }
+    // Copy Constructor
+    Hero(Hero& temp){
+        char *ch=new char[strlen(temp.name)+1];
+        strcpy(ch,temp.name);
+        this->name=ch;
+        
+        this->health=temp.health;
+        this->level=temp.level;
+    }
+    // Destructor
+    ~Hero(){
+        cout<<"Destructor"<<endl;
+    }
+    
+    void sethealth(int h){
+        health=h;
+    }
+    
+    void setlevel(char ch);
+    
+    int gethealth();
+    
+    char getlevel(){
+        return level;
+    }
+    void print(){
+        cout<<"[ Name: "<<this->name<<" ,";
+        cout<<"Health: "<<gethealth()<<" ,";
+        cout<<"Level: "<<getlevel()<<" ]"<<endl;
+    }
+    void setname(char name[]);
+};
+
+void Hero::setname(char name[]){
+    strcpy(this->name,name);
+}
+
+void Hero::setlevel(char ch){
+    level=ch;
+}
+
+int Hero::gethealth(){
+    return health;
+}
+
+
+int main(){
+    
+    Hero hero1;
+    hero1.sethealth(12);
+    hero1.setlevel('D');
+    char name[4]="xyz";
+    hero1.setname(name);
+    hero1.print();
+    
+    Hero hero2(hero1);
+    hero2.print();
+    hero1.name[0]='G';
+    hero1.print();
+    hero2.print();
+    
+    return 0;
+}
