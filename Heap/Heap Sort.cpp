badc40fe0d73cf93634fdc96f7664482988c76ba
Sanjay <121040100+sanjay-1458@users.noreply.github.com>
2023-10-15 14:05:04 +0530

    Update Heap Sort.cpp

--- Heap/Heap Sort.cpp
+++ Heap/Heap Sort.cpp
@@ -1,14 +1,7 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-void swap1(int *x,int *y){
-    int temp=*x;
-    *x=*y;
-    *y=temp;
-}
-
-void heapify(int n,int i,int arr[]){
+void heapify(int arr[],int n,int i){
     int largest=i;
     int left=2*i+1,right=2*i+2;
     if(left<n && arr[left]>arr[largest]){
@@ -18,26 +11,26 @@ void heapify(int n,int i,int arr[]){
         largest=right;
     }
     if(largest!=i){
-        swap1(&arr[largest],&arr[i]);
-        heapify(n,largest,arr);
+        swap(arr[i],arr[largest]);
+        heapify(arr,n,largest);
     }
 }
 
-void maxheap(int n,int arr[]){
+void maxheap(int arr[],int n){
     for(int i=n/2-1;i>=0;--i){
-        heapify(n,i,arr);
+        heapify(arr,n,i);
     }
 }
 
-void heapsort(int n,int arr[]){
-    maxheap(n,arr);
+void heapsort(int arr[],int n){
+    maxheap(arr,n);
     for(int i=n-1;i>=0;--i){
-        swap1(&arr[0],&arr[i]);
-        heapify(i,0,arr);
+        swap(arr[0],arr[i]);
+        heapify(arr,i,0);
     }
 }
 
-void print(int n,int arr[]){
+void print(int arr[],int n){
     for(int i=0;i<n;++i){
         cout<<arr[i]<<" ";
     }
@@ -46,12 +39,10 @@ void print(int n,int arr[]){
 
 int main()
 {
-    int arr[]={33,221,333,14,10,9,8,2,22,1000};
+    int arr[]={10,12,33,1,0,984,2};
     int n=sizeof(arr)/sizeof(arr[0]);
-    cout<<"Before heap sort"<<endl;
-    print(n,arr);
-    heapsort(n,arr);
-    cout<<"After heap sort"<<endl;
-    print(n,arr);
+    print(arr,n);
+    heapsort(arr,n);
+    print(arr,n);
     return 0;
 }
