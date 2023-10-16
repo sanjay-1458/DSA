55f6c57463c391c9bda869909632f627effebb88
Sanjay <121040100+sanjay-1458@users.noreply.github.com>
2023-10-16 19:11:25 +0530

    Update Kth Largest Element in an Array.cpp

--- Heap/Kth Largest Element in an Array.cpp
+++ Heap/Kth Largest Element in an Array.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int> pq;
+        priority_queue<int,vector<int>,greater<int>> pq;
         for(int i=0;i<nums.size();++i){
+            if(pq.size()==k && nums[i]<pq.top()){
+                continue;
+            }
             pq.push(nums[i]);
-        }
-        k--;
-        while(k--){
-            pq.pop();
+            if(pq.size()>k){
+                pq.pop();
+            }
         }
         return pq.top();
     }
